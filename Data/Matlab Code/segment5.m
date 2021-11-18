4cb19f8a619d21f1975cfdbb75d1fbbe5d20022a
hanyeob <2015100670@khu.ac.kr>
2021-11-18 11:40:47 +0900

    Update segment5.m
    threshold update

--- Data/Matlab Code/segment5.m
+++ Data/Matlab Code/segment5.m
@@ -1,13 +1,15 @@
 function segment5(INPUT)
     NEW_DATA = INPUT;
-
+    threshold = 0.2;
     tablename = inputname(1);
     j=1; next=1; fileNum=1;
     while j < height(NEW_DATA) 
-        while NEW_DATA.Velocity(j) == NEW_DATA.Velocity(j+next) && j+next<height(NEW_DATA) 
+        while (NEW_DATA.Velocity(j)+threshold) >= NEW_DATA.Velocity(j+next) ...
+               && NEW_DATA.Velocity(j+next) >= (NEW_DATA.Velocity(j)-threshold) ...
+               && j+next<height(NEW_DATA) 
             next=next+1;
         end
-        filename = sprintf('F1\\%sseg%d_%.2f.csv',tablename, fileNum, NEW_DATA.Velocity(j));
+        filename = sprintf('F1_02\\%sseg%d_%.2fkmh.csv',tablename, fileNum, NEW_DATA.Velocity(j));
         writetable(NEW_DATA(j:j+next-1,:),filename);
         fileNum = fileNum+1;
         j = j+next;
